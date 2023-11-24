65633461feb17b0fe47431e24bf3e70b7db5ed3e
ybbh <guohua16@outlook.com>
2023-11-24 10:53:47 +0800

    scupt-util

--- src/message.rs
+++ src/message.rs
@@ -5,7 +5,10 @@ use std::marker::Send;
 use bincode::{Decode, Encode};
 use serde::{Deserialize, Serialize};
 use serde::de::DeserializeOwned;
+use std::collections::HashSet;
+use tracing::error;
 
+use crate::serde_json_string::SerdeJsonString;
 use crate::error_type::ET;
 use crate::node_id::NID;
 use crate::res::Res;
@@ -125,4 +128,38 @@ impl <T:MsgTrait + 'static> MsgTrait for Vec<T> {
 
 }
 
+// utility for testing message
+pub fn test_check_message<M:MsgTrait + 'static>(m: M) -> bool {
+    let mut set = HashSet::new();
+    let r = serde_json::to_string_pretty(&m);
+    let s = if let Ok(s) = r {
+        s
+    } else {
+        error!("to json string error {:?}", m);
+        return false;
+    };
+
+    set.insert(m.clone());
+    let ss = SerdeJsonString::new(s);
+    let sv = ss.to_serde_json_value();
+    let v = sv.into_serde_json_value();
+    let m1 : M = serde_json::from_value(v).unwrap();
+    if !m1.eq(&m) {
+        error!("message is not equal {:?} {:?}", m1, m);
+        return false;
+    }
+
+    let ok = set.remove(&m1);
+    if !ok {
+        error!("no such message {:?}", m1);
+        return false;
+    }
+    let encoded = encode_message(m1.clone()).unwrap();
+    let (m2, _)= decode_message::<M>(encoded.as_slice()).unwrap();
+    if !m2.eq(&m) {
+        error!("message is not equal {:?} {:?}", m2, m);
+        return false;
+    }
+    return true;
+}
 
