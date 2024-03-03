e5029a2f3ad6a48842f35055d085676fbef7b0af
ybbh <guohua16@outlook.com>
2024-03-03 19:59:17 +0800

    scupt-util, Message::build_json

---
+++ src/test_message.rs
@@ -0,0 +1,13 @@
+
+#[cfg(test)]
+mod tests {
+    use crate::message::Message;
+
+    #[test]
+    fn test_message() {
+        let m1 = Message::new(1000, 1, 2);
+        let s = Message::<i32>::build_json(1000.to_string(), 1, 2).unwrap();
+        let m2:Message<i32> = serde_json::from_str(s.as_str()).unwrap();
+        assert!(m2.eq(&m1));
+    }
+}
